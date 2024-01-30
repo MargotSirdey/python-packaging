c820ac08b2d098810ecbb1c8566ed6aa41569c7a
Cristian Ruiz <cristian.ruiz@unil.ch>
2024-01-30 14:03:49 +0100

    Adapt solver to the tool's refactoring
    Move visualise to the right module, it is supposed to be
    one of the method exposed to users.

--- python-package/src/iceflow_try/solver.py
+++ python-package/src/iceflow_try/solver.py
@@ -1,29 +1,39 @@
+import matplotlib.pyplot as plt
+import matplotlib as mpl
 import numpy as np
 from . import tools
+
 
 def hello_world():
     print("Hello world !")
 
+
 def av(A):
     return 0.25 * (A[:-1, :-1] + A[:-1, 1:] + A[1:, :-1] + A[1:, 1:])
+
 
 def avx(A):
     return 0.5 * (A[:-1, :] + A[1:, :])
 
+
 def avy(A):
     return 0.5 * (A[:, :-1] + A[:, 1:])
+
 
 def compute_D(D, H, S, dSdx, dSdy, Snorm, a1, a2, dx, dy):
     dSdx = np.diff(S, axis=0) / dx
     dSdy = np.diff(S, axis=1) / dy
-    Snorm = np.sqrt(avy(dSdx)**2 + avx(dSdy)**2)
-    D[:] = ((a1 * av(H)**5) + (a2 * av(H)**3)) * Snorm**2
+    Snorm = np.sqrt(avy(dSdx) ** 2 + avx(dSdy) ** 2)
+    D[:] = ((a1 * av(H) ** 5) + (a2 * av(H) ** 3)) * Snorm**2
 
-def iceflow_solver(B, ELA, β, c, dx, dy, xc, yc):
+
+def iceflow_solver(m_balance_slope, m_balance_limiter, mean_height, data):
     # physics
     ρg = 910.0 * 9.81    # ice density x gravity
     dt = 0.1             # time step [yr]
     # numerics
+    B = tools.bedrock_elevation(data, mean_height)
+    ELA = tools.eq_line_altitude(data)
     nx, ny = B.shape      # numerical grid resolution
     nt = 1e4              # number of time steps
     nout = 1e3            # visu and error checking interval
@@ -46,10 +56,10 @@ def iceflow_solver(B, ELA, β, c, dx, dy, xc, yc):
     for it in range(int(nt)):
         np.copyto(H0, H)
         S = B + H
-        M = np.minimum(β * (S - ELA), c)
-        compute_D(D, H, S, dSdx, dSdy, Snorm, a1, a2, dx, dy)
-        qx[:] = avy(D) * np.diff(S[:, 1:-1], axis=0) / dx
-        qy[:] = avx(D) * np.diff(S[1:-1, :], axis=1) / dy
+        M = np.minimum(m_balance_slope * (S - ELA), m_balance_limiter)
+        compute_D(D, H, S, dSdx, dSdy, Snorm, a1, a2, data.dx, data.dy)
+        qx[:] = avy(D) * np.diff(S[:, 1:-1], axis=0) / data.dx
+        qy[:] = avx(D) * np.diff(S[1:-1, :], axis=1) / data.dy
         H[1:-1, 1:-1] = np.maximum(H[1:-1, 1:-1] + dt * (np.diff(qx, axis=0) + np.diff(qy, axis=1) + M[1:-1, 1:-1]), 0.0)
         if it % nout == 0:
             # error checking
@@ -58,9 +68,39 @@ def iceflow_solver(B, ELA, β, c, dx, dy, xc, yc):
             print(f"it = {it}, err = {err:.3e}")
             if err < ϵ:
                 break
-    return H, S, B, xc, yc
+    return H, S
+"""
+    visualise(H, S, B, xc, yc)
+
+Visualise bedrock and ice elevation.
+"""
+def visualise(H, S, B, xc, yc):
+    S_v = np.copy(S)
+    S_v[H <= 0.01] = np.nan
+    fig = plt.figure(figsize=(100, 45))
+    axs = fig.add_subplot(121, projection='3d')
+    axs.set_xlabel("x [km]")
+    axs.set_ylabel("y [km]")
+    axs.set_zlabel("elevation [m]")
+    xic, yic = np.meshgrid(xc, yc)
+    axs.set_box_aspect((4, 4, 1))
+    axs.view_init(azim=25)
+    p1 = axs.plot_surface(xic / 1e3, yic / 1e3, B, rstride=1, cstride=1, cmap='viridis', edgecolor='none')
+    p2 = axs.plot_surface(xic / 1e3, yic / 1e3, S_v, rstride=1, cstride=1, cmap='viridis', edgecolor='none')
+    norm = mpl.colors.Normalize(vmin=0, vmax=6000)
+    fig.colorbar(mpl.cm.ScalarMappable(norm=norm, cmap='viridis'),
+             ax=axs, orientation='vertical', label='H ice [m]', shrink=0.5)
+
+    plt.tight_layout()
+    plt.show()
+
 
 if __name__ == "__main__":
     # run the code and visualize the output
     resol = 256
-    tools.visualise(*iceflow_solver(*tools.create_data(resol, resol)))
+    m_balance_slope = 0.01
+    m_balance_limiter = 2.0
+    mean_height = 3500 # mean height [m]
+    data = tools.Data(resol, resol)
+    H, S = iceflow_solver(m_balance_slope, m_balance_limiter, mean_height, data)
+    visualise(H, S, tools.bedrock_elevation(data, mean_height), data.xc, data.yc)
